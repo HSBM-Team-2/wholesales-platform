43cfb92e1381b92a65f626810e7e65ef345b4b36
Bereket Yosef <134799825+brkt1@users.noreply.github.com>
2024-10-12 20:59:56 +0300

    start

---
+++ wholesale/src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://rdhsmvmatruicbsfaijq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkaHNtdm1hdHJ1aWNic2ZhaWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NTM5NTcsImV4cCI6MjA0NDMyOTk1N30.FMOIrcnAC7Cxg-gsprNSykUvocPJv6VqWKJwY12UnOQ';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
